1e17f50ad5d0241de5312c712dad22f7579a062b
ahmedElsayedHussien <ahbezo4@gmail.com>
2026-07-16 10:59:32 +0300

    cleartrash

--- test_login.py
+++
@@ -1,8 +0,0 @@
-import os
-import django
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.base')
-django.setup()
-
-from django.conf import settings
-print('LOGIN_URL is:', settings.LOGIN_URL)
